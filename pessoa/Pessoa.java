e3c581d029ad7da90250e82a9afb66d30c0e73f2
Rafael Parrela de Oliveira <RafaelParrela2005@hotmail.com>
2024-10-03 14:36:10 +0000

    Meus exemplos em POO

---
+++ pessoa/Pessoa.java
@@ -0,0 +1,13 @@
+package pessoa;
+
+public class Pessoa {
+    private String nome = "Ana";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
